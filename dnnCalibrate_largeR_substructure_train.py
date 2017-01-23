38cb8c4b429305f052fb98c71aadd3a9cc7b702e
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2017-01-23 16:01:35 -0500

    added E calibration

--- dnnCalibrate_largeR_substructure_train.py
+++ dnnCalibrate_largeR_substructure_train.py
@@ -36,23 +36,29 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=2)
 def create_model():
    model_pT = Sequential()
    model_pT.add( Dense( n_input_pT, input_dim=n_input_pT ) )
+   model_pT.add( Dense(50, activation='linear'))
    model_pT.add( Dense(20, activation='linear'))
-   model_pT.add( Dense(10, activation='linear'))
    model_pT.add( Dense(1, activation='linear'))
 
    model_eta = Sequential()
    model_eta.add( Dense( n_input_eta, input_dim=n_input_eta ) )
+   model_eta.add( Dense(50, activation='linear'))
    model_eta.add( Dense(20, activation='linear'))
-   model_eta.add( Dense(10, activation='linear'))
    model_eta.add( Dense(1, activation='linear'))
+
+   model_E = Sequential()
+   model_E.add( Dense( n_input_E, input_dim=n_input_E ) )
+   model_E.add( Dense(50, activation='linear'))
+   model_E.add( Dense(20, activation='linear'))
+   model_E.add( Dense(1, activation='linear'))
 
    model_M = Sequential()
    model_M.add( Dense( n_input_M, input_dim=n_input_M ) )
+   model_M.add( Dense(50, activation='linear'))
    model_M.add( Dense(20, activation='linear'))
-   model_M.add( Dense(10, activation='linear'))
    model_M.add( Dense(1, activation='linear'))
 
-   merged = Merge( [ model_pT, model_eta, model_M ], mode='concat' )
+   merged = Merge( [ model_pT, model_eta, model_E, model_M ], mode='concat' )
 
    model = Sequential()
    model.add(merged)
@@ -88,14 +94,22 @@ poly = PolynomialFeatures(2)
 df_training = pd.read_csv( training_filename, delimiter=',', names=header )
 #print df_training
 
+# transverse momentum
 features_pT = [ "fjet1_nocalib_pt", "fjet1_nocalib_eta", "fjet1_nocalib_E", "fjet1_nocalib_P", "fjet1_nocalib_M",
                 "fjet1_D2", "fjet1_C2", "fjet1_Tau1", "fjet1_Tau2", "fjet1_Tau3", "fjet1_Split12", "fjet1_Split23", "fjet1_Split34" ] 
 X_train_pT = df_training[features_pT].values
 
+# (pseudo)rapidity
 features_eta = [ "fjet1_nocalib_pt", "fjet1_nocalib_eta", "fjet1_nocalib_E", "fjet1_nocalib_P", "fjet1_nocalib_M",
                  "fjet1_D2", "fjet1_C2", "fjet1_Tau1", "fjet1_Tau2", "fjet1_Tau3", "fjet1_Split12", "fjet1_Split23", "fjet1_Split34" ]
 X_train_eta = df_training[features_eta].values
 
+# energy
+features_E  = [ "fjet1_nocalib_pt", "fjet1_nocalib_eta", "fjet1_nocalib_E", "fjet1_nocalib_P", "fjet1_nocalib_M",
+                "fjet1_D2", "fjet1_C2", "fjet1_Tau1", "fjet1_Tau2", "fjet1_Tau3", "fjet1_Split12", "fjet1_Split23", "fjet1_Split34" ]
+X_train_E = df_training[features_E].values
+
+# mass
 features_M  = [ "fjet1_nocalib_pt", "fjet1_nocalib_eta", "fjet1_nocalib_E", "fjet1_nocalib_P", "fjet1_nocalib_M",
                 "fjet1_D2", "fjet1_C2", "fjet1_Tau1", "fjet1_Tau2", "fjet1_Tau3", "fjet1_Split12", "fjet1_Split23", "fjet1_Split34" ]
 X_train_M = df_training[features_M].values
@@ -104,16 +118,17 @@ y_train = df_training[ [ "alpha_pt", "alpha_eta", "alpha_E", "alpha_M" ] ].value
 
 n_input_pT  = len( features_pT )
 n_input_eta = len( features_eta )
+n_input_E   = len( features_E )
 n_input_M   = len( features_M )
 
 print "INFO: N inputs pT: ", n_input_pT
 print "INFO: N inputs eta:", n_input_eta
+print "INFO: N inputs E:  ", n_input_E
 print "INFO: N inputs M:  ", n_input_M
 
+dnn = KerasRegressor( build_fn=create_model, nb_epoch=50, batch_size=100, verbose=1 )
 
-dnn = KerasRegressor( build_fn=create_model, nb_epoch=20, batch_size=1000, verbose=1 )
-
-dnn.fit( [ X_train_pT, X_train_eta, X_train_M ], y_train )
+dnn.fit( [ X_train_pT, X_train_eta, X_train_E, X_train_M ], y_train )
 
 dnn.model.save( "dnn.largeR_substructure.h5" )
 
