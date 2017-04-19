2bf2072753fa633deff40f3d42910c71d9d9d58f
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2017-04-19 16:21:14 -0400

    added script to convert Joe ntuples to csv files

---
+++ make_training_largeR_jennis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+target=csv/user.sketabch.akt10.csv
+rm -fr $target
+touch $target
+
+head -n   100000 csv/sketabch/Pythia8EvtGen_JZ3W.txt >> $target.tmp
+head -n  1000000 csv/sketabch/Pythia8EvtGen_JZ4W.txt >> $target.tmp
+head -n   500000 csv/sketabch/Pythia8EvtGen_JZ5W.txt >> $target.tmp
+
+#cat csv/sketabch/Pythia8EvtGen_JZ3W.txt >> $target.tmp
+#cat csv/sketabch/Pythia8EvtGen_JZ4W.txt >> $target.tmp
+#cat csv/sketabch/Pythia8EvtGen_JZ5W.txt >> $target.tmp
+
+shuf $target.tmp > $target
+#cat $target.tmp > $target
+
+rm $target.tmp
+
+echo "INFO: created training file $target"
