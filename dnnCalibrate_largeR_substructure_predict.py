25a1c64db032e8347a2b9f83817f575bb4bd4b4b
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2017-04-20 13:21:42 -0400

    added script to convert Joe ntuples to csv files

--- dnnCalibrate_largeR_substructure_predict.py
+++ dnnCalibrate_largeR_substructure_predict.py
@@ -89,7 +89,7 @@ from features import *
 df_testing = pd.read_csv( testing_filename, delimiter=',', names=header )
 #print df_testing
 
-event_info = df_testing[ [ "mc_weight" ] ].values
+event_info = df_testing[ [ "mc_Weight" ] ].values
 
 X_test_pT = df_testing[features_pT].values
 X_test_pT = scaler_pT.fit_transform( X_test_pT )
